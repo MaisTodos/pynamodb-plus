1cb8d2d091066f8325fb27ef0e05e3f9489d9444
muriloviana <murilo.vianamo@gmail.com>
2020-04-30 12:21:09 -0300

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pynamodb-plus
+
+[![PyPi Version](https://img.shields.io/pypi/v/pynamodb-plus.svg)](https://pypi.python.org/pypi/pynamodb-plus/)
+[![PyPi Python Versions](https://img.shields.io/pypi/pyversions/pynamodb-plus.svg)](https://pypi.python.org/pypi/pynamodb-plus/)
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
 Python 3 library with some utilities to use with PynamoDB
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    README = readme_file.read()
+
+setup(
+    name="pynamodb-plus", # Replace with your own username
+    version="0.0.1",
+    author="Murilo Viana",
+    author_email="murilo.vianamo@gmail.com",
+    description="Python 3 library with some utilities to use with PynamoDB",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaisTodos/pynamodb-plus",
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False,
+    keywords="python pynamodb plus",
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
